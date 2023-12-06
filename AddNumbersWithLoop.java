91215467cf01a21e03978ed8b4369f975543f7e3
Rahul Raparthi <108227043+raahul017@users.noreply.github.com>
2023-12-06 11:52:55 +0530

    Update AddNumbersWithLoop.java

--- AddNumbersWithLoop.java
+++ AddNumbersWithLoop.java
@@ -1,1 +1,29 @@
+package com.learning;
 
+import java.util.Scanner;
+
+public class AddNumbersWithLoop {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        
+        do {
+        	
+            System.out.print("Enter first number: ");
+            int a = sc.nextInt();
+            
+            System.out.print("Enter second number: ");
+            int b = sc.nextInt();
+            
+            //Addition of two numbers
+            int sum = a + b;
+            System.out.println("Sum of Two Numbers is: " + sum);
+            //press 1 to continue the process
+            System.out.println("Press 1 to perform addition again, Press any key to terminate");
+            choice = sc.nextInt();
+            
+        } while (choice == 1);
+
+        System.out.println("End of the program.....");
+    }
+}
